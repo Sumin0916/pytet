1522e960a6a0d85dc1653f4fe588da28cd8d12a9
Sumin0916 <72787222+Sumin0916@users.noreply.github.com>
2020-11-19 03:44:27 +0900

    Add files via upload

---
+++ pytet.py
@@ -0,0 +1,215 @@
+from matrix import *
+import random
+
+def rotate(blk):
+    if blk == [ [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ] ]:# |
+        return [ [ 0, 0, 0, 0 ], [ 1, 1, 1, 1 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 0, 0, 0 ], [ 1, 1, 1, 1 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ]:# ㅡ
+        return [ [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ] ]
+    ###
+    elif blk == [ [ 0, 0, 1, 0 ], [ 1, 1, 1, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ]:# ㄴ(좌우대칭)
+        return [ [ 0, 1, 0, 0 ], [ 0, 1, 0, 0 ], [ 0, 1, 1, 0 ], [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 1, 0, 0 ], [ 0, 1, 0, 0 ], [ 0, 1, 1, 0 ], [ 0, 0, 0, 0 ] ]:# ㄴ좌대 1번 
+        return [ [ 0, 0, 0, 0 ], [ 0, 1, 1, 1 ], [ 0, 1, 0, 0 ], [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 0, 0, 0 ], [ 0, 1, 1, 1 ], [ 0, 1, 0, 0 ], [ 0, 0, 0, 0 ] ]:# ㄴ좌대 2번 
+        return [ [ 0, 1, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 1, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 0, 0 ] ]:# ㄴ좌대 3번 
+        return [ [ 0, 0, 1, 0 ], [ 1, 1, 1, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ]
+    ### 
+    elif blk == [ [ 0, 1, 0, 0 ], [ 0, 1, 1, 1 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ]:# ㄴ
+        return [ [ 0, 1, 1, 0 ], [ 0, 1, 0, 0 ], [ 0, 1, 0, 0 ], [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 1, 1, 0 ], [ 0, 1, 0, 0 ], [ 0, 1, 0, 0 ], [ 0, 0, 0, 0 ] ]:# ㄴ1
+        return [ [ 0, 0, 0, 0 ], [ 0, 1, 1, 1 ], [ 0, 0, 0, 1 ], [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 0, 0, 0 ], [ 0, 1, 1, 1 ], [ 0, 0, 1, 0 ], [ 0, 0, 0, 0 ] ]:# ㄴ2
+        return [ [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 1, 1, 0 ], [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 1, 1, 0 ], [ 0, 0, 0, 0 ] ]:# ㄴ3
+        return [ [ 0, 1, 0, 0 ], [ 0, 1, 1, 1 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ]   
+    ###
+    elif blk == [ [ 0, 0, 1, 0 ],
+                  [ 0, 1, 1, 1 ], 
+                  [ 0, 0, 0, 0 ], 
+                  [ 0, 0, 0, 0 ] ]:# ㅗ
+        return [ [ 0, 0, 1, 0 ], 
+                 [ 0, 0, 1, 1 ], 
+                 [ 0, 0, 1, 0 ],
+                 [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 0, 1, 0 ], 
+                  [ 0, 0, 1, 1 ], 
+                  [ 0, 0, 1, 0 ], 
+                  [ 0, 0, 0, 0 ] ]:# ㅗ1
+        return [ [ 0, 0, 0, 0 ], 
+                 [ 0, 1, 1, 1 ], 
+                 [ 0, 0, 1, 0 ], 
+                 [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 0, 0, 0 ], 
+                  [ 0, 1, 1, 1 ], 
+                  [ 0, 0, 1, 0 ], 
+                  [ 0, 0, 0, 0 ] ]:# ㅗ2
+        return [ [ 0, 0, 1, 0 ], 
+                 [ 0, 1, 1, 0 ], 
+                 [ 0, 0, 1, 0 ], 
+                 [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 0, 1, 0 ], 
+                  [ 0, 1, 1, 0 ], 
+                  [ 0, 0, 1, 0 ],
+                  [ 0, 0, 0, 0 ] ]:# ㅗ3
+        return [ [ 0, 0, 1, 0 ], 
+                 [ 0, 1, 1, 1 ], 
+                 [ 0, 0, 0, 0 ], 
+                 [ 0, 0, 0, 0 ] ]
+    ###
+    elif blk == [ [ 0, 1, 1, 0 ],
+                  [ 0, 0, 1, 1 ],
+                  [ 0, 0, 0, 0 ],
+                  [ 0, 0, 0, 0 ] ]:#ㄱㄴ
+        return [ [ 0, 0, 1, 0 ],
+                 [ 0, 1, 1, 0 ],
+                 [ 0, 1, 0, 0 ],
+                 [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 0, 1, 0 ],
+                  [ 0, 1, 1, 0 ],
+                  [ 0, 1, 0, 0 ],
+                  [ 0, 0, 0, 0 ] ]:#ㄱㄴ1
+        return [ [ 0, 1, 1, 0 ],
+                 [ 0, 0, 1, 1 ],
+                 [ 0, 0, 0, 0 ],
+                 [ 0, 0, 0, 0 ] ]
+    ###
+    elif blk == [ [ 0, 1, 1, 0 ],
+                    [ 1, 1, 0, 0 ],
+                    [ 0, 0, 0, 0 ],
+                    [ 0, 0, 0, 0 ] ]:# ㄱㄴ 대칭
+        return [ [ 0, 1, 0, 0 ],
+                 [ 0, 1, 1, 0 ],
+                 [ 0, 0, 1, 0 ],
+                 [ 0, 0, 0, 0 ] ]
+    elif blk == [ [ 0, 1, 0, 0 ],
+                  [ 0, 1, 1, 0 ],
+                  [ 0, 0, 1, 0 ],
+                  [ 0, 0, 0, 0 ] ]:# ㄱㄴ 대칭
+        return [ [ 0, 1, 1, 0 ],
+                 [ 1, 1, 0, 0 ],
+                 [ 0, 0, 0, 0 ],
+                 [ 0, 0, 0, 0 ] ]
+    ###
+    elif blk == [ [ 0, 1, 1, 0 ],
+                  [ 0, 1, 1, 0 ], 
+                  [ 0, 0, 0, 0 ], 
+                  [ 0, 0, 0, 0 ] ]:# ㅁ
+        return blk
+    
+def draw_matrix(m):
+    array = m.get_array()
+    for y in range(m.get_dy()):
+        for x in range(m.get_dx()):
+            if array[y][x] == 0:
+                print("□", end='')
+            elif array[y][x] == 1:
+                print("■", end='')
+            else:
+                print("!!", end='')
+        print()
+arrayBlk_lst = [[ [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ] ],# |
+                [ [ 0, 0, 1, 0 ], [ 1, 1, 1, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ],# ㄴ 반대
+                [ [ 0, 1, 0, 0 ], [ 0, 1, 1, 1 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ],# ㄴ
+                [ [ 0, 0, 1, 0 ], [ 0, 1, 1, 1 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ],# ㅗ
+                [ [ 0, 1, 1, 0 ], [ 0, 0, 1, 1 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ],#ㄱㄴ
+                [ [ 0, 1, 1, 0 ], [ 1, 1, 0, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ],# ㄱㄴ 대칭
+                [ [ 0, 1, 1, 0 ], [ 0, 1, 1, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ]]# ㅁ     
+iScreenDy = 15
+iScreenDx = 10
+iScreenDw = 4
+top = 0
+left = iScreenDw + iScreenDx//2 - 2
+newBlockNeeded = False
+arrayScreen = [
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] ]
+iScreen = Matrix(arrayScreen)
+oScreen = Matrix(iScreen)
+cur_blc_lst = arrayBlk_lst[random.randrange(0,7)]
+currBlk = Matrix(cur_blc_lst)
+tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+tempBlk = tempBlk + currBlk
+oScreen.paste(tempBlk, top, left)
+draw_matrix(oScreen); print()
+
+while True:
+    newBlockNeeded = False
+    key = input('Enter a key from [ q (quit), a (left), d (right), s (down), w (rotate),\'space key\' (drop) ] : ')
+    if key == 'q':
+        print('Game terminated...')
+        break
+    elif key == 'a': # move left
+        left -= 1
+    elif key == 'd': # move right
+        left += 1
+    elif key == 's': # move down
+        top += 1
+    elif key == 'w': # rotate the block clockwise
+        cur_blc_lst = rotate(cur_blc_lst)
+        currBlk = Matrix(cur_blc_lst)
+    elif key == ' ':
+        while(True):
+            top += 1
+            tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+            tempBlk = tempBlk + currBlk
+            if tempBlk.anyGreaterThan(1) == True:
+                top -= 1
+                newBlockNeeded = True
+                break
+    else:
+        print('Wrong key!!!')
+        continue
+    tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+    tempBlk = tempBlk + currBlk
+    if tempBlk.anyGreaterThan(1):
+        if key == 'a': # undo: move right
+            left += 1
+        elif key == 'd': # undo: move left
+            left -= 1
+        elif key == 's': # undo: move up
+            top -= 1
+            newBlockNeeded = True
+        elif key == 'w': # undo: rotate the block counter-clockwise
+            for i in range(3):
+                cur_blc_lst = rotate(cur_blc_lst)
+            currBlk = Matrix(cur_blc_lst)
+        tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+        tempBlk = tempBlk + currBlk
+
+    oScreen = Matrix(iScreen)
+    oScreen.paste(tempBlk, top, left)
+    draw_matrix(oScreen); print()
+
+    if newBlockNeeded:
+        print("새로운 블럭 생성!!!!!!")
+        iScreen = Matrix(oScreen)
+        top = 0
+        cur_blc_lst = arrayBlk_lst[random.randrange(0,7)]
+        currBlk = Matrix(cur_blc_lst)
+        tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+        tempBlk = tempBlk + currBlk
+        if tempBlk.anyGreaterThan(1):
+            print('Game Over!!!')
+            break
+        oScreen = Matrix(iScreen)
+        oScreen.paste(tempBlk, top, left)
+        draw_matrix(oScreen); print()
